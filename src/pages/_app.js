5b5c086a0ac4df3ea06a779ace1660dd0b97a78c
Csb-218 <96022278+Csb-218@users.noreply.github.com>
2023-08-15 14:18:43 +0530

    ...

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,14 @@
 import '@/styles/globals.css'
+import MainLayout from '../components/mainlayout/MainLayout';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <MainLayout >
+        <Component {...pageProps} />
+    </MainLayout>
+    
+    </>
+
+  )
 }
